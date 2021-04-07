09b47b873ce9c9ca5b571767640898711f817410
You know who <harshitsh151@gmail.com>
2021-04-08 03:22:04 +0530

    'second-commit-with-added-Procfile'

--- uploaded-version/backend/router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res) => {
-    res.send("<h1>Server is up and running!</h1>");
-});
-
-module.exports = router;
